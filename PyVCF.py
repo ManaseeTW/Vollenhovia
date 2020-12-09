464df28da10e88ce99aad837a9d9ae76559277cf
ManaseeTW <44252244+ManaseeTW@users.noreply.github.com>
2020-12-09 12:58:57 +0530

    Update PyVCF.py

--- PyVCF.py
+++ PyVCF.py
@@ -1,5 +1,13 @@
 import vcf
+import pandas as pd
+
 vcf_reader = vcf.Reader(open('/Users/Manasee/Documents/Vollenhovia/filtered2.recode.snpEff.vcf', 'r'))
 for sample in range(2,5):
 	record = next(vcf_reader)
-for sample in record.samples: print(record.CHROM,record.POS,sample.sample,sample ['GT'],record.INFO['ANN']) 
+
+
+for sample in record.samples: 
+	print(record.CHROM,record.POS,sample.sample,sample ['GT'],record.INFO['ANN'])
+
+df = pd.DataFrame(record.samples)
+print(df)
