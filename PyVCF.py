b0d236651daf297ae3436a13df3854f60717e244
ManaseeTW <44252244+ManaseeTW@users.noreply.github.com>
2020-12-14 17:40:45 +0530

    Update PyVCF.py

--- PyVCF.py
+++ PyVCF.py
@@ -5,9 +5,16 @@ vcf_reader = vcf.Reader(open('/Users/Manasee/Documents/Vollenhovia/filtered2.rec
 for sample in range(2,5):
 	record = next(vcf_reader)
 
-
+list = []
 for sample in record.samples: 
-	print(record.CHROM,record.POS,sample.sample,sample ['GT'],record.INFO['ANN'])
+	print(list.append((record.CHROM,record.POS,sample.sample,sample ['GT'],record.INFO['ANN'])))
+print(list)
 
-df = pd.DataFrame(record.samples)
-print(df)
+
+
+df = pd.DataFrame([list])
+print(type(df))
+
+df2 = pd.DataFrame()
+df2 = df.append(pd.DataFrame([record.samples])).transpose()
+print(df2)
