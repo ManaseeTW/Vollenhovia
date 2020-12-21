0581beb5b2cd305d4ed62642004d88df41e3d115
ManaseeTW <44252244+ManaseeTW@users.noreply.github.com>
2020-12-21 22:16:16 +0530

    Update PyVCF.py

--- PyVCF.py
+++ PyVCF.py
@@ -1,20 +1,8 @@
-import vcf
 import pandas as pd
 
-vcf_reader = vcf.Reader(open('/Users/Manasee/Documents/Vollenhovia/filtered2.recode.snpEff.vcf', 'r'))
-for sample in range(2,5):
-	record = next(vcf_reader)
-
-list = []
-for sample in record.samples: 
-	print(list.append((record.CHROM,record.POS,sample.sample,sample ['GT'],record.INFO['ANN'])))
-print(list)
-
-
-
-df = pd.DataFrame([list])
-print(type(df))
-
-df2 = pd.DataFrame()
-df2 = df.append(pd.DataFrame([record.samples])).transpose()
-print(df2)
+table = pd.read_table('/Users/Manasee/Documents/Vollenhovia/filtered2.recode.snpEff.vcf',skiprows=59,header=None,usecols=[0,1,9],nrows=100000)
+print(table)
+table["new"]=table[9].str[0:3]
+print(table)
+count=table["new"].value_counts()
+print(count)
