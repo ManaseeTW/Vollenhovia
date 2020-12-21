cb3a186148934065ffffebe96a96df7a9a48f670
ManaseeTW <44252244+ManaseeTW@users.noreply.github.com>
2020-12-21 22:32:34 +0530

    Update PyVCF.py

--- PyVCF.py
+++ PyVCF.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-table = pd.read_table('/Users/Manasee/Documents/Vollenhovia/filtered2.recode.snpEff.vcf',skiprows=59,header=None,usecols=[0,1,9],nrows=100000)
+table = pd.read_table('/Users/Manasee/Documents/Vollenhovia/filtered2.recode.snpEff.vcf',skiprows=59,header=None,usecols=[0,1,9],nrows=10000000)
 print(table)
 table["new"]=table[9].str[0:3]
 print(table)
